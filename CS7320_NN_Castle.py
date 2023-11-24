669fc5c72cb53e0e12041046ca735d93ccbbd936
James Castle <jamescastle@Jamess-MacBook-Pro.local>
2023-11-23 21:53:22 -0500

    init commit

---
+++ CS7320_NN_Castle.py
@@ -0,0 +1,20 @@
+# Name: James Castle
+# Class: CS 7320 Sec 401
+# Assignment 7: Neural Networks
+# This program represents the creation of a simple neural network that
+# predicts success (binary) based on a small set of input data.
+
+
+# Read in file
+
+# Data Wrangling
+
+# Create NN and define hyperparams
+
+# run NN
+
+# Print outputs for train-test accuracy
+
+
+
+
