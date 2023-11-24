08959cf9d2d4cfb74375d6877c772669a4882b28
James Castle <jamescastle@Jamess-MacBook-Pro.local>
2023-11-24 17:03:59 -0500

    add file read.

--- CS7320_NN_Castle.py
+++ CS7320_NN_Castle.py
@@ -4,8 +4,40 @@
 # This program represents the creation of a simple neural network that
 # predicts success (binary) based on a small set of input data.
 
+import torch
+import numpy as np
+import pandas as pd
+import csv
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
 
-# Read in file
+
+def read_file(file_name):
+    try:
+        with open(file_name, 'r') as file:
+            data = list(csv.reader(file, delimiter=','))
+            file.close()
+            data = np.array(data)
+            return data
+
+    except FileNotFoundError:
+        return []
+
+def data_cleaning(data):
+    # delete lines with null values
+    pass
+    # replace non-numrical data with numerical values
+
+def main():
+    file_name = "7320.finaldata.csv"
+    # Read in file
+    raw_data = read_file(file_name)
+    print(raw_data.shape)
+
+
 
 # Data Wrangling
 
@@ -15,6 +47,6 @@
 
 # Print outputs for train-test accuracy
 
-
+main()
 
 
