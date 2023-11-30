e17ad0255778260459d7defdcadf6489d0e59e41
jjcastle1992 <77635923+jjcastle1992@users.noreply.github.com>
2023-11-29 23:22:37 -0600

    update model to use Pandas DF instead of Numpy array for ease of use. Add in steps to remove highly correlated features. Test revised model without correlated features.

--- CS7320_NN_Castle.py
+++ CS7320_NN_Castle.py
@@ -37,12 +37,18 @@ def main():
     print(df.info())  # verify state replaced with ints
     print(df.head(10))
 
-    dataset = df.to_numpy()
     seed = 0
     np.random.seed(seed)
 
-    X = dataset[:, 0:7]
-    Y = dataset[:, 7]
+    # dataset = df.to_numpy()
+    # X = dataset[:, 0:7]
+    # Y = dataset[:, 7]
+
+    # attempt to keep things as a pandas DF
+    X = df.drop(['success'], axis=1)
+    print(X.head())
+    Y = df['success']
+    print(Y.head())
 
     # 67% train, 33% test
     X_train, X_test, y_train, y_test = train_test_split(X, Y,
@@ -51,6 +57,7 @@ def main():
 
     # Create NN and define hyperparams
     model = keras.Sequential()
+    model.add(layers.Dense(16, activation='relu'))  # for Part A
     model.add(layers.Dense(16, input_shape=(7,), activation='relu'))  # for Part A
     model.add(layers.Dense(1, activation='sigmoid'))  # output layer
     print('Part A model constructed')
@@ -66,17 +73,66 @@ def main():
     toc = time.time()
     print(f'{(toc - tic):.04f} seconds elapsed.')
 
-    # Standardize Data for part B
+    # Standardize Data for part B1
     scaler = StandardScaler()
     X_train_std = scaler.fit_transform(X_train)
     X_test_std = scaler.transform(X_test)
 
 
 
-    print('-------------START PART B: STANDARDIZED TEST-------------')
+    print('-------------START PART B1: STANDARDIZED TEST-------------')
     tic = time.time()
     # fit model
     model.fit(X_train_std, y_train, validation_data=(X_test_std, y_test),
+              epochs=100, batch_size=32)
+    toc = time.time()
+    print(f'{(toc - tic):.04f} seconds elapsed.')
+
+    # Check for and remove highly correlated data
+    corrMatrix = df.corr()
+    sns.heatmap(corrMatrix, annot=True, cmap='viridis')
+    plt.show()
+
+    corrDf = df
+    print(corrDf.head())
+    corrDf = corrDf.drop(['f1', 'f4'], axis=1)  # drop highly corr
+    print(corrDf.head())
+
+    corrMatrix = corrDf.corr()
+    sns.heatmap(corrMatrix, annot=True, cmap='viridis')
+    plt.show()
+
+    # refit model on de-correlated dataset
+    X_corr = corrDf.drop(['success'], axis=1)
+    Y_corr = corrDf['success']
+    print(X_corr.head())
+    print(Y_corr.head())
+
+    # 67% train, 33% test on de-correlated feature dataset
+    X_train, X_test, y_train, y_test = train_test_split(X_corr,
+                                                        Y_corr,
+                                                        test_size=0.33,
+                                                        random_state=seed)
+    # Standardize Data with correlated features removed for part B2
+    scaler = StandardScaler()
+    X_train_std_corr = scaler.fit_transform(X_train)
+    X_test_std_corr = scaler.transform(X_test)
+
+    model_corr = keras.Sequential()
+    model_corr.add(layers.Dense(16, activation='relu'))  # for Part B
+    model_corr.add(layers.Dense(16, input_shape=(5,), activation='relu'))  # for Part B
+    model_corr.add(layers.Dense(1, activation='sigmoid'))  # output layer
+
+    # compile model
+    model_corr.compile(loss="binary_crossentropy", optimizer='adam',
+                  metrics=['accuracy'])
+    print('Part B2 model constructed')
+
+    print('--------START PART B2: DECORRELATED & STANDARDIZED TEST----')
+    tic = time.time()
+    # fit model
+    model_corr.fit(X_train_std_corr, y_train,
+              validation_data=(X_test_std_corr, y_test),
               epochs=100, batch_size=32)
     toc = time.time()
     print(f'{(toc - tic):.04f} seconds elapsed.')
