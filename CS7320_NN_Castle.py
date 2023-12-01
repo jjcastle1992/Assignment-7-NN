7c4e36c52dd3913fbea73dc0fad64ec6016aa3bf
jjcastle1992 <77635923+jjcastle1992@users.noreply.github.com>
2023-11-30 21:24:15 -0600

    Revise part A and B models to only have single hidden layer. Add Part C to have models that leverage a second 16 input hidden layer.

--- CS7320_NN_Castle.py
+++ CS7320_NN_Castle.py
@@ -3,19 +3,15 @@
 # Assignment 7: Neural Networks
 # This program represents the creation of a simple neural network that
 # predicts success (binary) based on a small set of input data.
-import keras
-import numpy.random
+
 import keras
 from keras import layers
 import numpy as np
 import pandas as pd
 import time
-import csv
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn import metrics
 from sklearn.preprocessing import StandardScaler
 
 
@@ -31,9 +27,11 @@ def main():
 
     df = df.dropna()  # Drop any rows with missing data
     print(df.info())  # have 3 columns (rank, state not in fl/int format
-    df.replace({'rank': {'LO': 0, 'MED': 1, 'HI': 2}}, inplace=True)
+    df.replace({'rank': {'LO': 0, 'MED': 1, 'HI': 2}},
+               inplace=True)
     print(df.info())  # verify rank replaced with ints.
-    df.replace({'state': {'CAL': 0, 'NY': 1, 'TX': 2}}, inplace=True)
+    df.replace({'state': {'CAL': 0, 'NY': 1, 'TX': 2}},
+               inplace=True)
     print(df.info())  # verify state replaced with ints
     print(df.head(10))
 
@@ -57,15 +55,16 @@ def main():
 
     # Create NN and define hyperparams
     model = keras.Sequential()
-    model.add(layers.Dense(16, activation='relu'))  # for Part A
-    model.add(layers.Dense(16, input_shape=(7,), activation='relu'))  # for Part A
-    model.add(layers.Dense(1, activation='sigmoid'))  # output layer
+    model.add(layers.Dense(16, input_shape=(7,),
+                           activation='relu'))
+    model.add(layers.Dense(1, activation='sigmoid'))
     print('Part A model constructed')
     
     # compile model
     model.compile(loss="binary_crossentropy", optimizer='adam',
                   metrics=['accuracy'])
 
+    print('--------START PART A: Single Layer no std or decorr DF----')
     tic = time.time()
     # fit model
     model.fit(X_train, y_train, validation_data=(X_test, y_test),
@@ -79,12 +78,14 @@ def main():
     X_test_std = scaler.transform(X_test)
 
 
+    # *************** PART B **********
 
     print('-------------START PART B1: STANDARDIZED TEST-------------')
     tic = time.time()
     # fit model
-    model.fit(X_train_std, y_train, validation_data=(X_test_std, y_test),
-              epochs=100, batch_size=32)
+    model.fit(X_train_std, y_train,
+              validation_data=(X_test_std, y_test), epochs=100,
+              batch_size=32)
     toc = time.time()
     print(f'{(toc - tic):.04f} seconds elapsed.')
 
@@ -119,23 +120,70 @@ def main():
     X_test_std_corr = scaler.transform(X_test)
 
     model_corr = keras.Sequential()
-    model_corr.add(layers.Dense(16, activation='relu'))  # for Part B
-    model_corr.add(layers.Dense(16, input_shape=(5,), activation='relu'))  # for Part B
-    model_corr.add(layers.Dense(1, activation='sigmoid'))  # output layer
+    model_corr.add(layers.Dense(16, input_shape=(5,),
+                                activation='relu'))  # for Part B
+    model_corr.add(layers.Dense(1, activation='sigmoid'))
 
     # compile model
     model_corr.compile(loss="binary_crossentropy", optimizer='adam',
-                  metrics=['accuracy'])
+                       metrics=['accuracy'])
     print('Part B2 model constructed')
 
     print('--------START PART B2: DECORRELATED & STANDARDIZED TEST----')
     tic = time.time()
     # fit model
     model_corr.fit(X_train_std_corr, y_train,
-              validation_data=(X_test_std_corr, y_test),
-              epochs=100, batch_size=32)
+                   validation_data=(X_test_std_corr, y_test),
+                   epochs=100, batch_size=32)
     toc = time.time()
     print(f'{(toc - tic):.04f} seconds elapsed.')
+
+    # *************** PART C ***************************************
+
+    # define a new model
+    modelC1 = keras.Sequential()
+    modelC1.add(layers.Dense(16, input_shape=(7,),
+                            activation='relu'))
+    modelC1.add(layers.Dense(16, activation='relu'))
+    modelC1.add(layers.Dense(1, activation='sigmoid'))
+
+    modelC1.compile(loss="binary_crossentropy", optimizer='adam',
+                   metrics=['accuracy'])
+    print('Part C1 model constructed')
+
+    print('----START PART C1: STANDARDIZED TEST w/ 2 hidden layers----')
+    # fit new model with 7 inputs
+    tic = time.time()
+    modelC1.fit(X_train_std, y_train,
+               validation_data=(X_test_std, y_test),
+               epochs=100, batch_size=32)
+    toc = time.time()
+    print(f'{(toc - tic):.04f} seconds elapsed.')
+
+
+    # Define Model C2 on 5 inputs (decorrelated) w/ 2 hidden layers
+    modelC2 = keras.Sequential()
+    modelC2.add(layers.Dense(16, input_shape=(5,),
+                             activation='relu'))
+    modelC2.add(layers.Dense(16, activation='relu'))
+    modelC2.add(layers.Dense(1, activation='sigmoid'))
+
+    modelC2.compile(loss="binary_crossentropy", optimizer='adam',
+                    metrics=['accuracy'])
+    print('Part C2 model constructed')
+
+    print('---START PART C2: DECORRELATED & STD w/ 2 hidden layers----')
+
+    # fit new model with 5 feature inputs (decorrelated)
+    tic = time.time()
+    modelC2.fit(X_train_std_corr, y_train,
+                validation_data=(X_test_std_corr, y_test),
+                epochs=100, batch_size=32)
+    toc = time.time()
+    print(f'{(toc - tic):.04f} seconds elapsed.')
+
+    # C3 Revisit correlation (Drop only F4)
+
 
     print('done')
 
